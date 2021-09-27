36a01d04a315b9b66d78794b36d27c9365851c06
Polina S <makeyourstanding@gmail.com>
2021-09-27 22:54:14 +0300

    upd gitignore

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,4 +1,5 @@
-import types.Type;
+import app.*;
+import app.types.Type;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -6,18 +7,19 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        Database db = new Database("db1");
+        Database db = new Database();
         Schema schema = new Schema(
                 Arrays.asList(new Column("col1", Type.INTEGER), new Column("col2", Type.REAL)));
         ArrayList<Row> data = new ArrayList<Row>(Arrays.asList(
                 new Row(schema, new ArrayList<Cell>(
                         Arrays.asList(new Cell<Integer>(1), new Cell<Double>(2.9))))));
 
-        db.add(new Table("t1", schema, data));
-        db.add(new Table("t2", schema, data));
+        db.add(new Table("t3", schema, data));
+        db.add(new Table("t4", schema, data));
         Table t1 = db.get("t1");
         Row row = t1.getRow(0);
         System.out.println(db.getName() + ' ' + t1.getName());
         System.out.println(row.getCell(1).toString());
+        db.save();
     }
 }
